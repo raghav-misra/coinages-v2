4fae25a4fe8929f269b74510b42deb3162101c1a
Raghav Misra <raghav.m2014@gmail.com>
2020-04-24 15:32:27 -0700

    Restructured UI, looks hot

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,101 @@
 <template>
-    <div class="no-padding">
-        <NavBar />
+    <div class="no-padding" id="app">
+        <!-- Logo & Title -->
+        <header>
+            <router-link :to="{ name: 'StarterPage' }">
+                <h1 class="mono" id="title">
+                    Coin
+                    <img id="logo" src="@/assets/img/coins/dollar.png">
+                    AGES
+                </h1>
+            </router-link>
 
-        <router-view />
+            <!-- Money Display -->
+            <h3 class="font-weight-reg">
+                Net Worth:
+                <span id="moneyDisp" class="mono thin">
+                    ${{ money }}
+                </span>
+            </h3>
+        </header> 
+
+        <!-- Current Page -->
+        <div id="current-view">
+            <transition name="slide">
+                <router-view />
+            </transition>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import NavBar from '@/components/NavBar.vue';
 
 export default Vue.extend({
-    components: { NavBar }
+    computed: {
+        money() { return this.$store.state.money / 100; }
+    },
+    methods: {
+        profit() { this.$store.commit("profit"); }
+    }
 });
 </script>
 
 <style src="@/assets/css/reset.css"></style>
+
+<style scoped>
+/* Header Styles */
+header { 
+    padding: 0;
+    padding-bottom: 1rem;
+    margin: 0;
+}
+
+img#logo {
+    width: 6.25rem;
+    height: 6.25rem;
+    margin-left: 1rem;
+}
+
+h1#title {
+    padding: 0;
+    margin: 0;
+    font-size: 2.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-transform: uppercase;
+}
+
+a { 
+    text-decoration: none; 
+    padding: 0; margin: 0;
+}
+
+/* Money Display */
+#moneyDisp {
+    padding: 0.5rem;
+    background: var(--success);
+    border-radius: 2rem; 
+}
+
+/* SPA Styles */
+#current-view {
+    padding: 0; margin: 0;
+    position: relative;
+}
+
+#current-view > section {
+    position: absolute;
+    top: 0; left: 0;
+    right: 0; bottom: 0;
+}
+
+/* Slide between pages */
+.slide-enter-active, .slide-leave-active { transition: transform 1s ease-in-out, opacity 1s ease-in-out; }
+
+.slide-enter, .slide-leave-to { transform: rotateY(-90deg); opacity: 0;  }
+
+.slide-enter-to, .slide-leave { transform: rotateY(0); opacity: 1; }
+</style>
 
--- src/views/StarterPage.vue
+++ src/views/StarterPage.vue
@@ -1,17 +1,43 @@
 <template>
-    <div>
-        Company Mission
-    </div>
+    <section class="no-padding">
+        <router-link :to="{ name: 'FactoryPage' }">Factory <i class="fas fa-building"></i></router-link>
+        <router-link :to="{ name: 'RnDPage' }">Laboratory <i class="fas fa-flask"></i></router-link>
+        <router-link :to="{ name: 'StockPage' }">Wall Street <i class="fas fa-traffic-light"></i></router-link>
+        <router-link :to="{ name: 'MarketingPage' }">Marketing <i class="fas fa-ad"></i></router-link>
+    </section>
 </template>
+
+<style scoped>
+section {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+}
+
+a {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.25rem;
+    text-decoration: none;
+    padding: 1rem;
+    margin: 0;
+    transition: background 250ms ease-in-out;
+}
+
+a:hover { background: var(--accent); }
+</style>
 
 <script lang="ts">
 import Vue from "vue";
 
 export default Vue.extend({
-    
+    computed: {
+        money() { return this.$store.state.money / 100; }
+    },
+    methods: {
+        profit() { this.$store.commit("profit"); }
+    }
 });
 </script>
-
-<style scoped>
-
-</style>

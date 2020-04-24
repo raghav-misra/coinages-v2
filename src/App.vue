f8c0b44d90defa01d774f7f3bc190de798796fed
Raghav Misra <raghav.m2014@gmail.com>
2020-04-23 20:39:21 -0700

    Yay Vuex is cool

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
-
+        <h1>Current Money: {{ money }}</h1>
+        <button @click="profit(4)">Add Money</button>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
-    
+    computed: {
+        money() { return this.$store.state.money; }
+    },
+    methods: {
+        profit(amount?: number) {
+            this.$store.commit("profit", amount);
+        }
+    }
 });
 </script>
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,33 @@
 import Vuex from "vuex";
+import Vue from "vue";
 
-export default new Vuex.Store({
+Vue.use(Vuex);
+
+const coinValues: Record<string, number> = {
+    penny: 1,
+    nickel: 5, 
+    dime: 10,
+    quarter: 25,
+    halfDollar: 50,
+    dollar: 100,
+    world: 1000
+}
+
+const store = new Vuex.Store({
     state: {
+        money: 0,
+        currentCoin: "penny"
+    },
+    mutations: {
+        profit(state, amount?: number) {
+            if (amount) state.money += amount as number;
+            else state.money += coinValues[state.currentCoin]; 
+        },
         
+        loss(state, amount: number) { state.money -= amount; },
+
+        changeCoin(state, newCoin) { state.currentCoin = newCoin; }
     }
 });
+
+export default store;

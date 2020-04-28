3981e65c35f62487fcabfb06634d6ed6a30ebd3d
Raghav Misra <raghav.m2014@gmail.com>
2020-04-27 23:03:08 -0700

    Fix caching of asset files.

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,14 @@
-var assets = [
-    "/",
-    "/index.html",
-    "/js/chunk-vendors.js",
-    "/js/app.js"
-];
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+});
 
-self.addEventListener("install", installEvent => installEvent.waitUntil(
-    caches.open("coinages-cache").then(cache => cache.addAll(assets))
-));
+self.addEventListener('message', (event) => {
+    if (event.data.type === 'CACHE_URLS') {
+        event.waitUntil(
+            caches.open("coinages-cache")
+                .then( (cache) => {
+                    return cache.addAll(event.data.payload);
+                })
+        );
+    }
+});
